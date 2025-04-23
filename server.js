bfff771a4902006f4392caefafc5f6efef93439a
chamussolaDev <159538875+FernandoChamussola@users.noreply.github.com>
2025-04-23 12:00:51 +0200

    delete botwhat

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import devedorRoutas from './src/routes/devedorRoutas.js';
 import dividaRoutas from './src/routes/dividaRoutas.js';
 import pagamentoRoutas from './src/routes/pagamentoRoutas.js';
 import dashboardRoutas from './src/routes/dashboardRoutas.js';
-import { configurarTarefasAgendadas } from './src/services/notificacao.js';
+
 
 const app = express();
 
@@ -30,5 +30,4 @@ app.use('/api/relatorios', dashboardRoutas);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-    configurarTarefasAgendadas();
 })
